2fbd38049c10873d796f26cf762cfb25024ce20c
xin.shen <flw_r@163.com>
2015-11-13 08:40:56 +0800

    Add comment for skynet_daemon

--- skynet/skynet-src/skynet_daemon.c
+++ skynet/skynet-src/skynet_daemon.c
@@ -11,19 +11,32 @@
 static int
 check_pid(const char *pidfile) {
 	int pid = 0;
+
+  // open process id file, if fail just return 0
 	FILE *f = fopen(pidfile,"r");
 	if (f == NULL)
 		return 0;
+
+  // read process id from the file and then close file
 	int n = fscanf(f,"%d", &pid);
 	fclose(f);
 
+  // if read fail or read process id is 0 or is equal to current process id, return 0
 	if (n !=1 || pid == 0 || pid == getpid()) {
 		return 0;
 	}
-
+  
+  // int kill(pid_t pid, int sig):
+  // 1. if `sig` is 0 (the null signal), error checking is performed but no signal is actually sent.
+  // 2. the null signal can be used to check the validity of `pid`.
+  // 3. this function will return 0 if success
+  // 4. ESRCH: no process or process group can be found corresponding to that specified by `pid`
+  
+  // check whether `pid` is valid or not, if not valid then return 0
 	if (kill(pid, 0) && errno == ESRCH)
 		return 0;
 
+  // if `pid` is actully valid then return this `pid` back.
 	return pid;
 }
 
@@ -66,8 +79,9 @@ write_pid(const char *pidfile) {
 
 int
 daemon_init(const char *pidfile) {
+  // check stored process id is valid or not
+  // if it is valid then indicate the process is already running, just return
 	int pid = check_pid(pidfile);
-
 	if (pid) {
 		fprintf(stderr, "Skynet is already running, pid = %d.\n", pid);
 		return 1;
@@ -76,6 +90,15 @@ daemon_init(const char *pidfile) {
 #ifdef __APPLE__
 	fprintf(stderr, "'daemon' is deprecated: first deprecated in OS X 10.5 , use launchd instead.\n");
 #else
+  // int daemon(int nochdir, int noclose):
+  // 1. this function is for programs wishing to detach themselves form the controlling terminal
+  // and run in the background as system daemons.
+  // 2. if `nochdir` is 0, it changes the calling process's current working directory
+  // to the root directory (/); otherwise, the current working directory is left unchanged
+  // 3. if `noclose` is 0, it redirects stdin stdout stderr to `/dev/null`, 
+  // otherwise, no changes are made to these file descriptors
+  
+  // daemonize and keep current working directory and redirect standand stream to `/dev/null`.
 	if (daemon(1,0)) {
 		fprintf(stderr, "Can't daemonize.\n");
 		return 1;
